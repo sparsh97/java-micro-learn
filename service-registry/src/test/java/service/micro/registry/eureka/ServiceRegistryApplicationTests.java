fca5675ee4460f437591456896953dfaea13e330
Sparsh Verma <sparshvermadmc@gmail.com>
2024-06-22 16:21:24 +0530

    Added Eureka Server Registry

---
+++ service-registry/src/test/java/service/micro/registry/eureka/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package service.micro.registry.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
