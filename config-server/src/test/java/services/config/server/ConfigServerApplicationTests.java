1264484806747d8244c5619ea49102704723ae6b
Sparsh Verma <sparshvermadmc@gmail.com>
2024-06-23 12:42:58 +0530

    config-server added and every other service fetching config from central config

---
+++ config-server/src/test/java/services/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package services.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
